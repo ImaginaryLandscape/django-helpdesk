0fc9e92eb10e319e537c06375580f9213a6122b8
Steven Hemmy <stevenbhemmy@gmail.com>
2023-06-29 20:37:31 -0500

    preopoulate fields based on user profile data

--- helpdesk/views/abstract_views.py
+++ helpdesk/views/abstract_views.py
@@ -19,6 +19,27 @@ class AbstractCreateTicketMixin():
         for qpf in query_param_fields:
             initial_data[qpf] = request.GET.get(qpf, initial_data.get(qpf, ""))
 
+        # Ugly hack to prepopulate UIC user data into custom form fields
+        form_class = self.get_form_class()
+        if hasattr(form_class, 'user_field_map'):
+            user_map_items = form_class.user_field_map.items()
+            user = self.request.user
+            for ticket_field, user_field in user_map_items:
+                initial_data[ticket_field] = getattr(user, user_field)
+        if hasattr(form_class, 'profile_field_map') and hasattr(u, 'dashboarduserprofile'):
+            profile_map_items = form_class.profile_field_map.items()
+            user = self.request.user
+            for ticket_field, profile_field in profile_map_items:
+                if isinstance(profile_field, tuple):
+                    # this deals with mapping mismatched radios/selects to
+                    # corresponding equivalent value
+                    profile_value = getattr(user.dashboarduserprofile, profile_field[0])
+                    custom_val_map = profile_field[1]
+                    custom_value = custom_val_map.get(profile_value)
+                    initial_data[ticket_field] = custom_value
+                else:
+                    initial_data[ticket_field] = getattr(user.dashboarduserprofile, profile_field)
+
         return initial_data
 
     def get_form_kwargs(self, *args, **kwargs):
